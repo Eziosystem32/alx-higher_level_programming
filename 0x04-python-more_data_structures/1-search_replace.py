ee6d3741a35488ad94f9c81e43ea9a37010af2d9
Eziosystem32 <teen2000titans@gmail.com>
2023-03-15 18:20:27 +0300

    Mission completex

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """replaces all occurrences of an element by another in a new list."""
+    return list(map(lambda e: replace if e == search else e, my_list))
