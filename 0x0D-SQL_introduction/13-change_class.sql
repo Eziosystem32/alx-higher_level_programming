fdf0e13eafbab8a5cbdb3599c683811f09f5a1bf
Eziosystem32 <teen2000titans@gmail.com>
2023-05-16 19:25:34 +0300

    mission complete

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- script that deletes score <= 5 in the table second_table.
+
+DELETE FROM second_table WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- script that calculates the average score in the table second_table.
+
+SELECT AVG(score) AS average FROM second_table;
